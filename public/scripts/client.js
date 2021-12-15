987f63092643222c52ab0e432af944a9d40d5150
samsam3268 <samskleung@gmail.com>
2021-12-15 15:12:45 -0800

    updated client.js to be able to loop through an array of tweets and append them to index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 146124613220
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $(document).ready(function() {
+      $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    });
+  }
+};
+
+
 const createTweetElement = function(tweetData) {
-  return $(`
+  let $tweet = $(`
 <article>
   <header>
     <div class="profileAndName">
@@ -16,7 +56,7 @@ const createTweetElement = function(tweetData) {
   </header>
   <p>${tweetData.content.text}</p>
   <footer>
-    <div>${tweetData.created_at}</div>
+    <div>${timeago.format(tweetData.created_at)}</div>
     <div class="socials">
       <a class="fas fa-flag"></a>
       <a class="fas fa-retweet"></a>
@@ -25,26 +65,8 @@ const createTweetElement = function(tweetData) {
   </footer>
 </article>
 `);
-}
+  return $tweet;
+};
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-$(document).ready(function() {
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-});
+renderTweets(data);
