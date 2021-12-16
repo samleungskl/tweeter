1dc5d825d8f139f9a507e8d22f11053c4aaa0439
samsam3268 <samskleung@gmail.com>
2021-12-15 21:46:52 -0800

    updated write a new tweet button to be toggleable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,49 +3,33 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 146124613220
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+
+    // clear previous infos, otherwise it would duplicate
+    $('#tweets-container').html('');
+
+    for (let tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+      $(' tweetContent').text();
+    }
+  };
 
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (let tweet of tweets) {
-    let $tweet = createTweetElement(tweet);
-    $(document).ready(function() {
-      $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-    });
-  }
-};
-
-
-const createTweetElement = function(tweetData) {
-  let $tweet = $(`
+  const createTweetElement = function(tweetData) {
+    //const test = ;
+    let $tweet = $(`
 <article>
   <header>
     <div class="profileAndName">
@@ -54,7 +38,7 @@ const createTweetElement = function(tweetData) {
   </div>
     <div class="userHandle">${tweetData.user.handle}</div>
   </header>
-  <p>${tweetData.content.text}</p>
+  <p class="tweetContent">${escape(tweetData.content.text)}</p>
   <footer>
     <div>${timeago.format(tweetData.created_at)}</div>
     <div class="socials">
@@ -65,8 +49,67 @@ const createTweetElement = function(tweetData) {
   </footer>
 </article>
 `);
-  return $tweet;
-};
+    return $tweet;
+  };
+
+  const loadTweets = function() {
+    $.ajax({
+      type: "GET",
+      url: `/tweets`,
+    })
+      .then((response) => {
+        console.log('response = ', response);
+        renderTweets(response);
+      })
+      .catch((error) => {
+        console.log('error = ', error);
+      })
+  }
+  loadTweets()
+
+  $("#tweetButton").submit(function(event) {
+    // prevent the submit button from submitting
+    event.preventDefault();
+    // get the length of string inside textarea
+    let textAreaLength = Number($('textarea#tweet-text').val().length)
+    // check if textArea is empty
+    if (textAreaLength === 0) {
+      console.log('Path 1')
+      $(".message").html('Message cannot be empty.');
+      $('.alert').slideDown()
+      // check if character count is under 140
+    } else if (textAreaLength > 140) {
+      console.log('Path 2')
+      $(".message").html('Message cannot contain more than 140 characters.');
+      $('.alert').slideDown()
+    } else {
+      //if everything passed, post to server
+      $(".message").html('');
+      $('.alert').slideUp()
+      console.log('Path 3')
+      console.log('this = ', $(this).serialize())
+      const data = $(this).serialize();
+      $.ajax({
+        type: "POST",
+        url: `/tweets`,
+        data: data,
+      })
+        .then((response) => {
+          console.log('response = ', response)
+          loadTweets()
+        })
+        .catch((error) => {
+          console.log('error = ', error)
+        })
+      //clear textarea
+      $("#tweet-text").val('');
+      //reset counter
+      $("#counter").val('140');
+    }
+  });
 
 
-renderTweets(data);
+  $( ".newTweetButton" ).click(function() {
+    $('.new-tweet').slideToggle()
+  });
+});
